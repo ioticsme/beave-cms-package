db892f14496dcdc5939fd21aa3bc7b352f859daf
Ebrahim Nayakkan <ebru.nkn@gmail.com>
2024-10-31 09:06:08 +0400

    app config

--- controller/config/appSettings.controller.js
+++ controller/config/appSettings.controller.js
@@ -20,7 +20,7 @@ const list = async (req, res) => {
     //     console.log(configs.schema.path(p)?.instance || 'Mixed')
     // }
     return res.render('admin-njk/config/app-settings/listing', {
-        schema_fields: Config.schema,
+        // schema_fields: Config.schema,
         configs,
     })
 }
@@ -28,19 +28,21 @@ const list = async (req, res) => {
 const save = async (req, res) => {
     // console.log(req.body)
     const schema = Joi.object({
-        general: Joi.object({
-            client_name: Joi.string().required(),
-            frontend_url: Joi.string().optional(),
-            push_notification: Joi.boolean(),
-            slack: Joi.boolean(),
-            user_email_verification: Joi.boolean(),
-        }),
-        imagekit: Joi.object().optional(),
-        firebase: Joi.object().optional(),
-        slack: Joi.object().optional(),
-        // dir: Joi.string().required().valid('ltr', 'rtl'),
-        // is_default: Joi.boolean().optional(),
-        // id: Joi.optional(),
+        client_name: Joi.string().required(),
+        frontend_url: Joi.string().optional(),
+        user_email_verification: Joi.boolean().optional().allow(null, ''),
+        local_upload: Joi.boolean().optional().allow(null, ''),
+        ik_public_key: Joi.string().optional().allow(null, ''),
+        ik_private_key: Joi.string().optional().allow(null, ''),
+        ik_url: Joi.string().optional().allow(null, ''),
+        ik_folder: Joi.string().optional().allow(null, ''),
+        ik_active: Joi.boolean().optional().allow(null, ''),
+        cdry_api_key: Joi.string().optional().allow(null, ''),
+        cdry_api_secret: Joi.string().optional().allow(null, ''),
+        cdry_cloud_name: Joi.string().optional().allow(null, ''),
+        cdry_folder: Joi.string().optional().allow(null, ''),
+        cdry_active: Joi.boolean().optional().allow(null, ''),
+        default_drive: Joi.string().optional().allow(null, '').valid('imagekit', 'cloudinary'),
     })
 
     const validationResult = schema.validate(req.body, {
@@ -53,27 +55,37 @@ const save = async (req, res) => {
 
     try {
         await Config.deleteMany()
-        const config = await Config.create(req.body)
-        // console.log(config)
-        globalModuleConfig = {
-            // has_booknow: config.general?.has_booknow || false,
-        }
-
-        if (config?.general?.push_notification && config?.firebase?.admin_web) {
-            createFcmSwJS(config.firebase)
-            globalModuleConfig.firebase = config.firebase || false
-        } else {
-            globalModuleConfig.firebase = false
-        }
-
-        // await Config.updateOne(
-        //     {
-        //         _id: configRow.id,
-        //     },
-        //     req.body
-        // )
-
-        return res.status(200).json('done')
+        await Config.create({
+            general: {
+                client_name: req.body.client_name,
+                frontend_url: req.body.frontend_url,
+                user_email_verification: req.body.user_email_verification,
+            },
+            media_drive: {
+                local_upload: req.body.local_upload,
+                imagekit: {
+                    public_key: req.body.ik_public_key,
+                    private_key: req.body.ik_private_key,
+                    url: req.body.ik_url,
+                    folder: req.body.ik_folder,
+                    default: req.body.default_drive == 'imagekit' ? true : false,
+                    active: req.body.ik_active,
+                },
+                cloudinary: {
+                    api_key: req.body.cdry_api_key,
+                    api_secret: req.body.cdry_api_secret,
+                    cloud_name: req.body.cdry_cloud_name,
+                    folder: req.body.cdry_folder,
+                    default: req.body.default_drive == 'cloudinary' ? true : false,
+                    active: req.body.cdry_active,
+                },
+            },
+        })
+        // return res.status(200).json('done')
+        return res.status(200).json({
+            message: 'Config updated',
+            // url: `/config/app-settingss`,
+        })
     } catch (e) {
         // console.log(e)
         if (e.errors) {
--- model/Config.js
+++ model/Config.js
@@ -2,11 +2,6 @@ const { mongoose, Schema } = require('mongoose')
 
 const ConfigSchema = new mongoose.Schema(
     {
-        order_no: {
-            type: Number,
-            default: 1000,
-            Comment: `This will be the next order no. Cahnge with cautious!!!`,
-        },
         general: {
             client_name: {
                 type: String,
@@ -16,93 +11,31 @@ const ConfigSchema = new mongoose.Schema(
                 type: String,
                 // required: true,
             },
-            push_notification: {
-                type: Boolean,
-                default: false,
-            },
-            slack: {
-                type: Boolean,
-                default: false,
-            },
             user_email_verification: {
                 type: Boolean,
                 default: false,
             },
         },
-        imagekit: {
-            public_key: {
-                type: String,
-                default: null,
-                // required: [true, 'Required for media storage'],
-            },
-            private_key: {
-                type: String,
-                default: null,
-                // required: [true, 'Required for media storage'],
-            },
-            url: {
-                type: String,
-                default: null,
-                // required: [true, 'Required for media storage'],
-            },
-            folder: {
-                type: String,
-                default: null,
-                // required: [true, 'Required for media storage'],
-            },
-        },
-        firebase: {
-            admin_web: {
+        media_drive: {
+            local_upload: {
                 type: Boolean,
                 default: false,
             },
-            user: {
-                type: Boolean,
-                default: false,
+            imagekit: {
+                public_key: String,
+                private_key: String,
+                url: String,
+                folder: String,
+                default: Boolean,
+                active: Boolean,
             },
-            apiKey: {
-                type: String,
-                required: [function () {
-                    return this.general.push_notification
-                }, 'Required if you need push_notification'],
-            },
-            authDomain: {
-                type: String,
-                required: [function () {
-                    return this.general.push_notification
-                }, 'Required if you need push_notification'],
-            },
-            projectId: {
-                type: String,
-                required: [function () {
-                    return this.general.push_notification
-                }, 'Required if you need push_notification'],
-            },
-            storageBucket: {
-                type: String,
-                required: [function () {
-                    return this.general.push_notification
-                }, 'Required if you need push_notification'],
-            },
-            messagingSenderId: {
-                type: String,
-                required: [function () {
-                    return this.general.push_notification
-                }, 'Required if you need push_notification'],
-            },
-            appId: {
-                type: String,
-                required: [function () {
-                    return this.general.push_notification
-                }, 'Required if you need push_notification'],
-            },
-        },
-        slack: {
-            webhook_url: {
-                type: String,
-                required: [function () {
-                    return this.general.slack
-                }, 'Required if you need slack'],
+            cloudinary: {
+                api_key: String,
+                api_secret: String,
+                cloud_name: String,
+                folder: String,
+                default: Boolean,
+                active: Boolean,
             },
         },
     },
